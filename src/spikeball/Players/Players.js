44f232c6cdc3a98e9271606d1c2c0bd450cb671c
maliawanderer <malia.wanderer@gmail.com>
2023-12-10 21:59:38 -0500

    import

--- src/spikeball/Players/Players.js
+++ src/spikeball/Players/Players.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { useParams} from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
 import * as client from "./client"
 
 function Players() {
