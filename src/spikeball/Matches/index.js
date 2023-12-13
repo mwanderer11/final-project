54a86308a5e67f2c0288e0301241d8f762a8c2f3
maliawanderer <malia.wanderer@gmail.com>
2023-12-13 11:41:22 -0500

    fix

--- src/spikeball/Matches/index.js
+++ src/spikeball/Matches/index.js
@@ -5,7 +5,6 @@ import {useEffect, useState} from "react";
 function Match() {
     const {id} = useParams();
     const [match, setMatch] = useState(null);
-    const navigate = useNavigate();
 
     const fetchMatch = async () => {
         const m = await client.findMatchById(id);
@@ -19,6 +18,9 @@ function Match() {
     return (
         <div>
             <h1>Match up</h1>
+            1. {match.player1}, {match.player2} vs {match.player3}, {match.player4}
+            2. {match.player1}, {match.player3} vs {match.player2}, {match.player4}
+            3. {match.player1}, {match.player4} vs {match.player3}, {match.player2}
         </div>
     )
 
